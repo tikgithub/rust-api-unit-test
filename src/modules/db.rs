cbda1dfcb93e7509971d1f7a81786a6be89c3bba
Mr. Vadsalasack VORLAVONGSOR <tik.github@gmail.com>
2025-11-05 18:01:24 +0700

    update

--- src/modules/db.rs
+++ src/modules/db.rs
@@ -1,5 +1,5 @@
 use async_trait::async_trait;
-use sqlx::{Pool, Postgres};
+use sqlx::{Error, Pool, Postgres};
 use crate::modules::models::User;
 
 #[derive(Debug, Clone)]
@@ -16,5 +16,37 @@ impl DBClient {
 #[async_trait]
 pub trait UserExt{
     async fn get_user()-> Result<Option<User>,sqlx::Error>;
-    
+    async fn get_users(&self,page:u32,limit:u32)->Result<Vec<User>,sqlx::Error>;
+    async fn save_user<T: Into<String> + Send>(
+        &self,
+        name: T,
+        email: T,
+        password: T,
+    )-> Result<User, sqlx::Error>;
+    async fn save_admin_user<T: Into<String> + Send>(
+        &self,
+        name: T,
+        email: T,
+        password:T,
+    )->Result<User, sqlx::Error>;
+
+
+    #[async_trait]
+    impl UserExt for DBClient{
+        async fn get_user() -> Result<Option<User>, Error> {
+            todo!()
+        }
+
+        async fn get_users(&self, page: u32, limit: u32) -> Result<Vec<User>, Error> {
+            todo!()
+        }
+
+        async fn save_user<T: Into<String> + Send>(&self, name: T, email: T, password: T) -> Result<User, Error> {
+            todo!()
+        }
+
+        async fn save_admin_user<T: Into<String> + Send>(&self, name: T, email: T, password: T) -> Result<User, Error> {
+            todo!()
+        }
+    }
 }
